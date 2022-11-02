5972ca9d51f89f9ff02e41967e96beed3498b4cf
GuilhermeCostaDF <guidocostadf@gmail.com>
2022-11-02 16:00:52 -0300

    att datafake

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -12,7 +12,7 @@ export const articleOthers = [
   {
     "id": "2",
     "photo": "https://blog.geekhunter.com.br/wp-content/uploads/2019/07/angular8-min-1.jpg",
-    "title": "Meu artigo",
+    "title": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer orcimassa, suscipit ut gravida sed",
     "description": "Mussum Ipsum, cacilds vidis litro abertis. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget. Paisis, filhis, espiritis santis. Quem num gosta di mé, boa gentis num é.Vehicula non. Ut sed ex eros. Vivamus sit amet nibh non tellus tristique interdum. A ordem dos tratores não altera o pão duris.Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis.Per aumento de cachacis, eu reclamis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi.",
   },
   {
