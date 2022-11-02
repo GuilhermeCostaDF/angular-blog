eb8263bb2a08a70d1695fe2a07aebb7ab565cbc0
GuilhermeCostaDF <guidocostadf@gmail.com>
2022-11-02 11:56:29 -0300

    content page stylized

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+  {
+    "id": 1,
+    "photo": "aaa",
+    "title": "aaaaaaaaaaaa",
+    "description": "aaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+  },
+
+  {
+    "id": 2,
+    "photo": "bbb",
+    "title": "bbbbbbbbbb",
+    "description": "bbbbbbbbbbbbbbbbbbbbbb",
+  }
+]
